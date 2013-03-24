0f06f20ae3ff30eb12b04befe37c6d96ef8e3bca
Michael Oswalt <mao47@case.edu>
2013-03-24 16:15:54 -0400

    Created a sleep mode activity for the data collection stuff. Not exactly sure if this is how it should be but it will work for now.

--- SmartAlarm/src/com/example/smartalarm/SleepModeActivity.java
+++ SmartAlarm/src/com/example/smartalarm/SleepModeActivity.java
@@ -2,27 +2,21 @@ package com.example.smartalarm;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
 
-public class ConfirmSleepAlarmActivity extends Activity {
+public class SleepModeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_confirm_sleep_alarm);
+		setContentView(R.layout.activity_sleep_mode);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.confirm_sleep_alarm, menu);
+		getMenuInflater().inflate(R.menu.sleep_mode, menu);
 		return true;
-	}
-	public void onConfirmAlarmClick(View view) {
-		Intent intent = new Intent(this, SleepModeActivity.class);
-		startActivity(intent);
 	}
 
 }
