c065f901195ef651f6376e436a0c5e04a232ea7e
Michael Oswalt <mao47@case.edu>
2013-03-24 22:57:18 -0400

    Pass alarm time values into sleep mode activity.

--- SmartAlarm/src/com/example/smartalarm/ConfirmSleepAlarmActivity.java
+++ SmartAlarm/src/com/example/smartalarm/ConfirmSleepAlarmActivity.java
@@ -5,9 +5,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TimePicker;
 
 public class ConfirmSleepAlarmActivity extends Activity {
-
+	
+	public static final String START_ALARM_HOURS = "com.example.smartalarm.START_ALARM_HOURS";
+	public static final String START_ALARM_MINUTES = "com.example.smartalarm.START_ALARM_MINUTES";
+	public static final String FAILSAFE_ALARM_HOURS = "com.example.smartalarm.FAILSAVE_ALARM_HOURS";
+	public static final String FAILSAFE_ALARM_MINUTES = "com.example.smartalarm.FAILSAFE_ALARM_MINUTES";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,7 +27,19 @@ public class ConfirmSleepAlarmActivity extends Activity {
 		return true;
 	}
 	public void onConfirmAlarmClick(View view) {
+		
+		TimePicker timepicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+		int timeHours = timepicker.getCurrentHour();
+		int timeMinutes = timepicker.getCurrentMinute();
+		TimePicker failsafePicker = (TimePicker) findViewById(R.id.alarmFailsafePicker);
+		int failsafeHours = failsafePicker.getCurrentHour();
+		int failsafeMinutes = failsafePicker.getCurrentMinute();
 		Intent intent = new Intent(this, SleepModeActivity.class);
+		intent.putExtra(START_ALARM_HOURS, timeHours);
+		intent.putExtra(START_ALARM_MINUTES, timeMinutes);
+		intent.putExtra(FAILSAFE_ALARM_HOURS, failsafeHours);
+		intent.putExtra(FAILSAFE_ALARM_MINUTES, failsafeMinutes);
+		
 		startActivity(intent);
 	}
 
--- SmartAlarm/src/com/example/smartalarm/SleepModeActivity.java
+++ SmartAlarm/src/com/example/smartalarm/SleepModeActivity.java
@@ -2,14 +2,24 @@ package com.example.smartalarm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class SleepModeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.activity_sleep_mode);
+		
+		Intent intent = getIntent();
+		
+		//The start time and end time for the alarm window are passed into the activity on its creation
+		int windowStartHours 	= intent.getIntExtra(ConfirmSleepAlarmActivity.START_ALARM_HOURS, 6);
+		int windowStartMinutes 	= intent.getIntExtra(ConfirmSleepAlarmActivity.START_ALARM_MINUTES, 30);
+		int windowEndHours 		= intent.getIntExtra(ConfirmSleepAlarmActivity.FAILSAFE_ALARM_HOURS, 7);
+		int windowEndMinutes 	= intent.getIntExtra(ConfirmSleepAlarmActivity.FAILSAFE_ALARM_HOURS, 30);
+		
 	}
 
 	@Override
