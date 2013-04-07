9aa50479ed43ec0d6f5e94f5feb93231990a0796
Sean <saf66@case.edu>
2013-04-07 18:24:29 -0400

    Added a basic preference framework

--- SmartAlarm/src/com/example/smartalarm/SettingsActivity.java
+++ SmartAlarm/src/com/example/smartalarm/SettingsActivity.java
@@ -6,7 +6,8 @@ import android.preference.PreferenceActivity;
 // https://developer.android.com/guide/topics/ui/settings.html
 public class SettingsActivity extends PreferenceActivity {
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
 	}
 }
